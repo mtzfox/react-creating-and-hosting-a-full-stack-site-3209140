f396b4efdc3f53994e98f5e18bded45a45778032
Mike <21209421+mtzfox@users.noreply.github.com>
2022-12-31 16:26:02 -0800

    Added express and get put requests

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,9 +1,45 @@
 import express from 'express';
 
-const app = express();
+let articlesInfo = [{
+    name: 'learn-react',
+    upvotes: 0,
+    comments: [],
+}, {
+    name: 'learn-node',
+    upvotes: 0,
+    comments: [],
+}, {
+    name: 'mongodb',
+    upvotes: 0,
+    comments: [],
+}]
 
-app.get('/hello', (req, res) => {
-    res.send('Hello!');
+const app = express();
+app.use(express.json());
+
+app.put('/api/articles/:name/upvote', (req, res) => {
+  const { name } = req.params;
+  const article = articlesInfo.find(a => a.name === name);
+  if (article)  {
+    article.upvotes += 1;
+    res.send(`The ${name} now has ${article.upvotes} upvotes`);
+  } else {
+    res.send('The article doesn\'t exist');
+  }
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const { postedBy, text } = req.body;
+    const { name } = req.params;
+
+    const article = articlesInfo.find(a => a.name === name);
+
+    if (article) {
+        article.comments.push({ postedBy, text });
+        res.send(article.comments);
+    } else {
+        res.send('The article doesn\'t exist');
+    }
 });
 
 app.listen(8000, () => {
